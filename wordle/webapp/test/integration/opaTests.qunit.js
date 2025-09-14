9a19e3d5118261ea19b79d92dd1a1f5ad5ac1a1b
Kunnak <yannicks@live.de>
2025-09-15 00:11:46 +0200

    Created VSCode Projekt

---
+++ wordle/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["wordle/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ wordle/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"wordle/test/unit/controller/Wordle.controller"
+], function () {
+	"use strict";
+});
